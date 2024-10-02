50586397efe8ff00a954574be98d4e1c1493d2ce
Abdallah94212 <abdallah.borji@epitech.digital>
2024-10-02 16:09:29 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,8 @@
+function capitalize(word) {
+return word.map( words => words.toUpperCase() );
+}
+
+// Here is some code to test and display your function output:
+// (you must modify it to test out your code but /!\ do not deliver it /!\ ). Only deliver your function
+const result = capitalize(["CHANGE", "this", "array", "BY", "ThE", "RESULT", "oF", "youR", "FUNCTION", "TO", "test", "IT", "OUT"])
+displayResult(result)
